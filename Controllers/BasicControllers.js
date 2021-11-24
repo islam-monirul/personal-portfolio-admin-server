fa572f319f45dba968cab5fbaf8bee02ebfd6d74
Monirul Islam <devmonir.uiu.cse@gmail.com>
2021-11-24 21:41:29 +0600

    server basic setup

---
+++ Controllers/BasicControllers.js
@@ -0,0 +1,3 @@
+export const basic = async (req, res) => {
+  return res.send("Welcome to the Server Side!");
+};
---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const { MongoClient } = require("mongodb");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// database connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9tg9f.mongodb.net/${process.env.DB_HOST}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    console.log("Connection Established");
+  } finally {
+    // await client.close();
+  }
+}
+
+run().catch(console.dir);
+
+app.get("/", async (req, res) => {
+  res.send("Welcome to the Server Admin!");
+});
+app.listen(port, () => {
+  console.log("Running on port : ", port);
+});
